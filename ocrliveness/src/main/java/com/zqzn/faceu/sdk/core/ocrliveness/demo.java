71963adfa5e108b09880ce05d748fb393e7cb1eb
qqwoshiwuli <qq362603030>
2019-03-28 17:22:04 +0800

    4.0

--- ocrliveness/src/main/java/com/zqzn/faceu/sdk/core/ocrliveness/demo.java
+++ ocrliveness/src/main/java/com/zqzn/faceu/sdk/core/ocrliveness/demo.java
@@ -2,5 +2,14 @@ package com.zqzn.faceu.sdk.core.ocrliveness;
 
 public class demo {
 
-
+        public LivenessEngine livenessEngine()
+        {
+            LivenessEngine livenessEngine = new LivenessEngine();
+            return livenessEngine;
+        }
+    public OcrEngine OcrEngine()
+    {
+        OcrEngine ocrEngine = new OcrEngine();
+        return ocrEngine;
+    }
 }
